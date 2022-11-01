d50d74a7e643986f3edb0c7a7d0ff4c92aa189a9
JAYANTH H S <102643623+JAYANTH5775@users.noreply.github.com>
2022-11-01 23:05:35 +0530

    day-1 of 100daysofcoding

---
+++ BandNameGenerator.py
@@ -0,0 +1,5 @@
+print("welcome to the band name Generator")  #greeting to the bandgenerator
+
+FirstName=input("Enter the city you grew up : \n\n")  #asking for the cityname
+SecondName= input("enter your pet name : \n\n")   #asking for the pet name
+print('your Musicband could be is :'+(FirstName +" "+SecondName))   #contenating the city  name and the pet name to generate the band name
